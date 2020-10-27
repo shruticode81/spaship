934e29e5d1b1d4913651939a5e3996f5c0286056
mwcz <mwc@clayto.com>
2020-10-27 12:45:45 -0400

    update env defaults

--- packages/manager/public/config/manager.js
+++ packages/manager/public/config/manager.js
@@ -6,8 +6,8 @@ window.SPAship = {
       environments: [
         {
           name: "Dev",
-          api: "http://localhost:8008/api/v1",
-          domain: "http://localhost:8765",
+          api: SPASHIP_LOCALENV_API || "http://localhost:2345/api/v1",
+          domain: SPASHIP_LOCALENV_DOMAIN || "http://localhost:1234",
         },
       ],
     },
